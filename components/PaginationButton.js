cfb61278de9e077152b5134d98f6f2a34c35f633
Carlos Aviña <carlosavilu11@gmail.com>
2021-10-20 22:02:15 -0500

    Add styles when pagination button is disabled

--- components/PaginationButton.js
+++ components/PaginationButton.js
@@ -1,11 +1,12 @@
-const PaginationButton = ({ children, onClick, selected, ...extraProps }) => {
+const PaginationButton = ({ children, onClick, selected, disabled, ...extraProps }) => {
   return (
     <button
       {...extraProps}
       onClick={onClick}
-      className={`bg-gray-300 hover:bg-gray-400 rounded-md text-lg text-black font-semibold px-3 py-1 ${
-        selected ? "bg-blue-600 text-white" : null
-      }`}
+      disabled={disabled}
+      className={`bg-gray-300 rounded-md text-lg text-black font-semibold px-3 py-1 ${
+        selected ? 'bg-blue-600 text-white' : null
+      } ${disabled ? 'cursor-not-allowed' : 'hover:bg-gray-400'}`}
     >
       {children}
     </button>
