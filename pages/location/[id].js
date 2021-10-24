8e58b70aa01a3d6959c75a08a87e6a90370db0ca
Carlos Aviña <carlosavilu11@gmail.com>
2021-10-24 13:33:47 -0500

    Remove SSG in location page

--- pages/location/[id].js
+++ pages/location/[id].js
@@ -1,16 +1,14 @@
 import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
-import { Card, BackButton } from 'components';
 
-import { getLocation, getLocationsIds } from 'queries';
+import { Card, BackButton, Loader } from 'components';
+import { getLocation } from 'queries';
 
-const Location = (props) => {
+const Location = () => {
   const router = useRouter();
 
-  const { data, isLoading, error } = useQuery(
-    ['location', router.query.id],
-    () => getLocation(router.query.id),
-    { initialData: props.location }
+  const { data, isLoading, error } = useQuery(['location', router.query.id], () =>
+    getLocation(router.query.id)
   );
 
   const { id, name, type, dimension } = data?.location || {};
@@ -19,15 +17,16 @@ const Location = (props) => {
     router.back();
   }
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>{error}</p>;
+  if (isLoading) return <Loader />;
+
+  if (error) return <p>{error.message}</p>;
 
   return (
     <main className="flex flex-col gap-4 p-5">
       <BackButton onClick={goBack} />
       <Card>
         <h1 className="text-3xl font-semibold">
-          {id}.{name}
+          {id}. {name}
         </h1>
         <p>
           <b>Type:</b> {type}
@@ -39,27 +38,5 @@ const Location = (props) => {
     </main>
   );
 };
-
-export async function getStaticProps({ params }) {
-  const location = await getLocation(params.id);
-
-  return {
-    props: {
-      location,
-    },
-  };
-}
-
-export async function getStaticPaths() {
-  const response = await getLocationsIds();
-  const paths = response.locations.results.map(({ id }) => ({
-    params: { id },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
 
 export default Location;
