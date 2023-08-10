26d2d7317e3b6bee2b5396f0ae52cd721f51f2cd
Carlos Aviña <juan.avina@wizeline.com>
2023-08-10 00:07:43 -0500

    Add locations page

--- queries/getLocation.ts
+++ queries/getLocation.ts
@@ -13,10 +13,43 @@ const GET_LOCATION = gql`
   }
 `;
 
+const GET_LOCATIONS = gql`
+  query getLocations($page: Int, $dimension: String, $name: String, $type: String){
+    locations (page: $page, filter: { dimension: $dimension, name: $name, type: $type }) {
+      info {
+        count
+        next
+        pages
+        prev
+      }
+      results {
+        id
+        name
+        dimension
+        type
+      }
+    }
+  }
+`
+
 function getLocation(id) {
   return request(gql_uri, GET_LOCATION, {
     id,
   });
 }
+type FilterType = {
+  dimension: String;
+  name: String;
+  type: String;
+}
 
-export default getLocation;
+function getLocations(page: number, {dimension, name, type}: FilterType) {
+  return request(gql_uri, GET_LOCATIONS, {
+    page,
+    dimension,
+    name,
+    type,
+  })
+}
+
+export { getLocation, getLocations };
--- queries/index.ts
+++ queries/index.ts
@@ -1,6 +1,6 @@
 import getCharacters from './getCharacters';
 import { getEpisodes, getEpisodesByIds } from './getEpisodes';
-import getLocation from './getLocation';
+import { getLocation, getLocations } from './getLocation';
 import getLocationsIds from './getLocationsIds';
 
-export { getCharacters, getEpisodes, getEpisodesByIds, getLocation, getLocationsIds };
+export { getCharacters, getEpisodes, getEpisodesByIds, getLocation, getLocationsIds, getLocations };
